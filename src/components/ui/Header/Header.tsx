1207b19e1c68abbabba6fe84134558d5391dc2d3
sushidesu <hugemerge@gmail.com>
2022-09-12 15:28:55 +0900

    fix styles

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { clsx } from "clsx"
+import { Link } from "rocon/react"
+import { routes } from "../../../app/Router"
 import styles from "./Header.module.css"
 
 export const Header = () => {
   return (
     <div className={clsx(styles["wrapper"])}>
-      <h1>Yaruzo</h1>
+      <Link route={routes.exactRoute} className={clsx(styles["title"])}>
+        <h1>Yaruzo</h1>
+      </Link>
     </div>
   )
 }
